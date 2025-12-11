b2e73f5fbea585217eaf8866dfab1b6fd7fb47b5
Maxim Bodyansky <bodyansky@gmail.com>
2025-12-11 17:03:28 +0300

    Add type casting to Redis method calls

--- backend/db.py
+++ backend/db.py
@@ -1,4 +1,4 @@
-from typing import Annotated, AsyncGenerator
+from typing import Annotated, AsyncGenerator, Awaitable, cast
 from fastapi import Depends
 from redis.asyncio import Redis
 
@@ -24,31 +24,33 @@ class AddressBook:
         self._redis = redis
 
     async def is_alive(self) -> bool:
-        return await self._redis.ping()
+        return await cast(Awaitable[bool], self._redis.ping())
 
     async def create(self, phone: str, address: str) -> str:
-        if not await self._redis.hsetnx(self.REDIS_KEY, phone, address):
+        if not await cast(
+            Awaitable[bool], self._redis.hsetnx(self.REDIS_KEY, phone, address)
+        ):
             raise ObjectAlreadyExists
 
         return address
 
     async def get(self, phone: str) -> str:
-        address = await self._redis.hget(self.REDIS_KEY, phone)
+        address = await cast(Awaitable[str], self._redis.hget(self.REDIS_KEY, phone))
         if address is None:
             raise ObjectNotFound
 
         return address
 
     async def update(self, phone: str, address: str):
-        if not await self._redis.hexists(self.REDIS_KEY, phone):
+        if not await cast(Awaitable[bool], self._redis.hexists(self.REDIS_KEY, phone)):
             raise ObjectNotFound
 
-        await self._redis.hset(self.REDIS_KEY, phone, address)
+        await cast(Awaitable[int], self._redis.hset(self.REDIS_KEY, phone, address))
 
         return address
 
     async def delete(self, phone: str):
-        if not await self._redis.hdel(self.REDIS_KEY, phone):
+        if not await cast(Awaitable[int], self._redis.hdel(self.REDIS_KEY, phone)):
             raise ObjectNotFound
 
 
